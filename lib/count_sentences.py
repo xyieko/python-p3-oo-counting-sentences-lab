afb02d3724c02b9f8a686eec126c0a22e49b93d9
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-05-30 19:20:40 +0300

    Add compiled files and update count_sentences.py

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
-#!/usr/bin/env python3
+import sys
+import io
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        else:
+            self._value = value
+
+    
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    
+    def is_question(self):
+        return self._value.endswith('?')
+
+    
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+   
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]+', self._value)
+        
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+
+        return len(sentences)
